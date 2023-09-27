34f0fef8a51e8871518740f0f9e02cae28fe331b
Kashyab19 <kashyabnarayanan3721@gmail.com>
2023-09-26 21:23:34 -0400

    Added React Helmet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { ComplexNavbar } from './components/Navbar';
-import LandingPage from './pages/LandingPage';
+import React from "react";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { ComplexNavbar } from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import { Helmet } from "react-helmet";
 
 library.add(fab, fas);
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>summerize.ai</title>
+        <meta
+          name="description"
+          content="summerize.ai is a browser extension designed to provide instant summaries of websites, articles, research papers, and more. Extract core ideas and save time as you browse the web."
+        />
+        <meta
+          name="keywords"
+          content="chrome extension, summerizer, AI summarizer, article summary, research paper summary, web page summarizer, SummarizeIt, NLP, browser extension"
+        />
+      </Helmet>
       <ComplexNavbar />
       <LandingPage />
     </div>
